400e55ccd7d5aceea76df50d54aa271ba82f5933
ohseju <tarryping@gmail.com>
2026-01-29 10:46:55 +0900

    feat: book 저장 로직, 테스트 코드 작성

--- src/main/java/com/yonsai/book/entity/Book.java
+++ src/main/java/com/yonsai/book/entity/Book.java
@@ -9,15 +9,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.yonsai.book.dto.BookDTO;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "books")
 public class Book {
 
@@ -32,5 +31,70 @@ public class Book {
 	@Column(nullable = false, precision = 10, scale = 2) // 전체 10자리, 소수점 2자리까지 허용
 	private BigDecimal price;
 	
+	
+	public BookDTO toDTO() {
+		BookDTO dto = new BookDTO();
+		dto.setTitle(this.title);
+		dto.setAuthor(this.author);
+		dto.setPrice(this.price);
+		return dto;
+	}
+	
+	public static Book from(BookDTO dto) {
+		Book book = new Book();
+		book.setId(dto.getId());
+		book.setTitle(dto.getTitle());
+		book.setAuthor(dto.getAuthor());
+		book.setPrice(dto.getPrice());
+		return book;
+	}
+	
+	
+	public Book() {
+	}
+
+	public Book(Long id, String title, String author, BigDecimal price) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", price=" + price + "]";
+	}
 	
 }
